29aaef56bec20e34df0deb48295e381565d46df7
haashim0 <103762873+haashim0@users.noreply.github.com>
2022-12-03 15:18:51 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -5,23 +5,23 @@ try:
 except ImportError:
     class Config:
         DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        API_HASH = [str, "620f0a2aa2cd1474a4953619b3e3643d"]
+        API_ID = [int, 14505719]
+        BOT_TOKEN = [str, "5945219564:AAHMjrNCDXTgfBT-HuBmxjDN_0RHOORYqbA"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
-        MAX_QUEUE_SIZE = [int, 5]
-        SLEEP_SECS = [int, 10]
-        IS_MONGO = [bool, False]
+        MAX_QUEUE_SIZE = [int, 10]
+        SLEEP_SECS = [int, 0]
+        IS_MONGO = [bool, true]
 
         # Access Restriction
-        IS_PRIVATE = [bool, False]
+        IS_PRIVATE = [bool, true]
         AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 0]
+        OWNER_ID = [int, 5291606032]
 
         # Public username url or invite link of private chat
-        FORCEJOIN = [str,""]
-        FORCEJOIN_ID = [int,-100123465978]
+        FORCEJOIN = [str,"@tamildub_linkzz"]
+        FORCEJOIN_ID = [int,-1001786200411]
 
         TRACE_CHANNEL = [int, 0]
 
